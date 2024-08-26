063547b6dc613229046af53a996558ed039649c6
Michał Białek <kontakt.michalbialek@gmail.com>
2024-08-26 12:36:59 +0200

    feat: Providing manual mapping CarHistory in CarToServiceMapper, by default method mapFromEntity

--- src/main/java/pl/bialek/infrastructure/database/repository/mapper/CarToServiceMapper.java
+++ src/main/java/pl/bialek/infrastructure/database/repository/mapper/CarToServiceMapper.java
@@ -5,14 +5,49 @@ import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 import pl.bialek.domain.CarHistory;
 import pl.bialek.domain.CarToService;
+import pl.bialek.domain.Part;
+import pl.bialek.domain.Service;
 import pl.bialek.infrastructure.database.entity.CarToServiceEntity;
+import pl.bialek.infrastructure.database.entity.ServiceMechanicEntity;
+import pl.bialek.infrastructure.database.entity.ServicePartEntity;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface CarToServiceMapper {
     @Mapping(target = "carServiceRequests", ignore = true)
     CarToService mapFromEntity(CarToServiceEntity entity);
 
-    CarHistory mapFromEntity(String vin, CarToServiceEntity entity);
+    default CarHistory mapFromEntity(String vin, CarToServiceEntity carToServiceEntity){
+        return CarHistory.builder()
+                .carVin(vin)
+                .carServiceRequests(carToServiceEntity.getCarServiceRequests().stream()
+                        .map(carServiceRequest -> CarHistory.CarServiceRequest.builder()
+                                .carServiceRequestNumber(carServiceRequest.getCarServiceRequestNumber())
+                                .receivedDateTime(carServiceRequest.getReceivedDateTime())
+                                .completedDateTime(carServiceRequest.getCompletedDateTime())
+                                .customerComment(carServiceRequest.getCustomerComment())
+
+                                .parts(carServiceRequest.getServiceParts().stream()
+                                        .map(ServicePartEntity::getPart)
+                                        .map(service -> Part.builder()
+                                                .serialNumber(service.getSerialNumber())
+                                                .description(service.getDescription())
+                                                .price(service.getPrice())
+                                                .build())
+                                        .toList())
+
+                                .services(carServiceRequest.getServiceMechanics().stream()
+                                        .map(ServiceMechanicEntity::getService)
+                                        .map(service -> Service.builder()
+                                                .serviceCode(service.getServiceCode())
+                                                .description(service.getDescription())
+                                                .price(service.getPrice())
+                                                .build())
+                                        .toList())
+
+                                .build())
+                        .toList())
+                .build();
+    }
 
     CarToServiceEntity mapToEntity(CarToService car);
 
